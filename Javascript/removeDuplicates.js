d4fa1a9763acf0bfcabd20817931b51f9bb4d9b9
Hernan Zamora <hernanzamora1999@gmail.com>
2024-02-19 09:43:57 -0300

    Remove Duplicates.2

--- Javascript/removeDuplicates.js
+++ Javascript/removeDuplicates.js
@@ -1,5 +1,17 @@
-const nums = [1,1,2];
+const nums = [1, 1, 2];
 
-const removeDuplicates = function(nums) {
-    
+var removeDuplicates = function (nums) {
+  let ptr1 = 0,
+    ptr2 = 1,
+    len = nums.length;
+
+  while (ptr2 < len) {
+    if (nums[ptr1] != nums[ptr2]) {
+      ptr1++;
+      nums[ptr1] = nums[ptr2];
+    }
+    ptr2++;
+  }
+
+  return ptr1 + 1;
 };
